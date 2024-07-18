3b6d871f4daf3b170d4d1f6dd99364d0adc04cb6
kangsancheon <rkdtkscjs1@naver.com>
2024-07-18 22:46:12 +0900

    footer_update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,19 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-section">
-          <h4>Use cases</h4>
+          {/* <h4>Use cases</h4>
           <p>UI design</p>
-          <p>Prototyping</p>
+          <p>Prototyping</p> */}
         </div>
         <div className="footer-section">
-          <h4>Explore</h4>
+          {/* <h4>Explore</h4>
           <p>Design</p>
-          <p>Development</p>
+          <p>Development</p> */}
         </div>
         <div className="footer-section">
-          <h4>Resources</h4>
+          {/* <h4>Resources</h4>
           <p>Blog</p>
-          <p>Best practices</p>
+          <p>Best practices</p> */}
         </div>
       </div>
     </footer>
